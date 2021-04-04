449f5fa8ca2062ebaa79e1b7a265ddd61e037d90
Russell Warwick <russell.warwick@gymshark.com>
2021-04-04 01:08:00 +0100

    Init

---
+++ SharkCollectionKit/App Delegate/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  SharkCollectionKit
+//
+//  Created by Russell Warwick on 04/04/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let winScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: winScene.coordinateSpace.bounds)
+        window?.windowScene = winScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
